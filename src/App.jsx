1bf67429ad053613152b725114e3f7979501924f
aniket-padyal <padyal10aniket@gmail.com>
2026-03-09 17:51:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <h1 className="font-bold text-5xl text-blue-700  ">aniket</h1>
+    </main>
+  );
+};
+
+export default App;
