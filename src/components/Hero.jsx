b9d3b1c8899998a3883e04e4772367606fbcf252
aniket-padyal <padyal10aniket@gmail.com>
2026-03-12 15:57:23 +0530

    implement video player in hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,42 @@
+import { useState, useRef } from "react";
+// import Video from "./Video";
+
 const Hero = () => {
+  const [currentIndex, setCurrentIndex] = useState(1);
+  const [hasClicked, setHasClicked] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedVideos, setLoadedVideos] = useState(0);
+
+  const totalVideos = 4;
+  const nextVideoRef = useRef(null);
+
+  const handleVideoLoad = () => {
+    setLoadedVideos((prev) => prev + 1);
+  };
+
+  const handleMiniVideoClick = () => {
+    setHasClicked(true);
+
+    setCurrentIndex((prevIndex) => prevIndex + 1);
+  };
+
+  const getVideoSrc = (index) => `/videos/hero-${index}.mp4`;
+
   return (
     <section id="hero">
       <div id="video-frame">
         <div>
           <div className="video-mask  ">
-            <div>MiniVideoPlayer</div>
+            <div onClick={handleMiniVideoClick} className="mini-video">
+              <video
+                loop
+                muted
+                ref={nextVideoRef}
+                src={getVideoSrc(currentIndex + 1)}
+                id="current-video"
+                onLoadedData={handleVideoLoad}
+              />
+            </div>
           </div>
         </div>
       </div>
